9d7e707cd93e1f2aedd82fdef12bd07fe01b3dd9
BerettaSM <ramonsaviato@hotmail.com>
2024-03-20 19:46:17 -0300

    feat: add deep clone utility function

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -3,6 +3,7 @@
 var Utils = (function () {
     var publicAPI = {
         clone,
+        deepClone,
     };
 
     return publicAPI;
@@ -12,6 +13,23 @@ var Utils = (function () {
     function clone(obj) {
         var prototype = Object.getPrototypeOf(obj);
         var clone = Object.assign(Object.create(prototype), obj);
+        return clone;
+    }
+
+    function _deepClone(obj) {
+        if (typeof obj !== 'object' || obj === null) {
+            return obj;
+        }
+        var copy = Array.isArray(obj) ? [] : {};
+        for (let [key, value] of Object.entries(obj)) {
+            copy[key] = _deepClone(value);
+        }
+        return copy;
+    }
+
+    function deepClone(obj) {
+        var clone = _deepClone(obj);
+        Object.setPrototypeOf(clone, Object.getPrototypeOf(obj));
         return clone;
     }
 })();
